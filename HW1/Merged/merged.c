2bd92b95fd2061871ad09e3f36d83223877ef73e
Paul Camarata <camarata@pc-dev.pc.local>
2018-07-21 12:02:31 -0700

    added merged changes

--- HW1/Merged/merged.c
+++ HW1/Merged/merged.c
@@ -15,13 +15,19 @@
 /**
  * Function prototypes
  */
-ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+ssize_t read_jiffies(struct file *file, char *buf, size_t count, loff_t *pos);
+ssize_t read_seconds(struct file *file, char *buf, size_t count, loff_t *pos);
 
-static struct file_operations proc_ops = {
+
+static struct file_operations proc_ops_jiffies = {
         .owner = THIS_MODULE,
-        .read = proc_read,
+        .read = read_jiffies,
 };
 
+static struct file_operations proc_ops_seconds = {
+        .owner = THIS_MODULE,
+        .read = read_seconds,
+};
 
 /* This function is called when the module is loaded. */
 int proc_init(void)
@@ -30,11 +36,13 @@ int proc_init(void)
         // creates the /proc/golden entry
         // the following function call is a wrapper for
         // proc_create_data() passing NULL as the last argument
-        proc_create(PROC1_NAME, 0, NULL, &proc_ops);
-        proc_create(PROC2_NAME, 0, NULL, &proc_ops);
+        proc_create(PROC1_NAME, 0, NULL, &proc_ops_jiffies);
+        proc_create(PROC2_NAME, 0, NULL, &proc_ops_seconds);
 
-        printk(KERN_INFO "/proc/%s created\n /proc/%s created\n Golden_Ratio = %lu\n", PROC1_NAME, PROC2_NAME, GOLDEN_RATIO_PRIME);
-        printk(KERN_INFO "%s\n", file->_f)
+        printk(KERN_INFO "/proc/%s created\n", PROC1_NAME);
+        printk(KERN_INFO "/proc/%s created\n", PROC2_NAME);
+        printk(KERN_INFO "Jiffies = %lu\n", jiffies);
+        printk(KERN_INFO "Golden_Ratio = %lu\n", GOLDEN_RATIO_PRIME);
 	return 0;
 }
 
@@ -66,7 +74,7 @@ void proc_exit(void) {
  * count:
  * pos:
  */
-ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+ssize_t read_jiffies(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
 {
         int rv = 0;
         char buffer[BUFFER_SIZE];
@@ -87,6 +95,26 @@ ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t 
         return rv;
 }
 
+ssize_t read_seconds(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+        int rv = 0;
+        char buffer[BUFFER_SIZE];
+        static int completed = 0;
+
+        if (completed) {
+                completed = 0;
+                return 0;
+        }
+
+        completed = 1;
+
+        rv = sprintf(buffer, "Seconds = %lu\n", jiffies/HZ);
+
+        // copies the contents of buffer to userspace usr_buf
+        copy_to_user(usr_buf, buffer, rv);
+
+        return rv;
+}
 
 /* Macros for registering module entry and exit points. */
 module_init( proc_init );
